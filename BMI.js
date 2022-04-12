334be61706bdb9c159f50f346660432de7039447
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-04-12 21:13:13 +0200

    Update BMI.js

--- BMI.js
+++ BMI.js
@@ -1,26 +1,50 @@
-const readline = require("readline").createInterface({
+const RL = require("readline");
+
+const readline = RL.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+/** @param {string} x @returns {string} */
+function ReadOnlyNumbers(x) {
+    const regexp = /[+-]?\d+(\.\d+)?/g;
+    return x.match(regexp)[0];
+}
+
+/** @param {string} x @returns {number} */
+function TurnIntoInt(x) {
+    const numbers = ReadOnlyNumbers(x);
+    if (numbers === null || numbers === "null") {
+        throw process.stdout.write("Provided value must be type of number but  the type of the value that was provided was a string.");
+    }
+    const i = parseFloat(ReadOnlyNumbers(x));
+
+    return i;
+}
+
+/** @param {number} str @returns {string} */
+function ReadSingularDecimal(str) {
+    const float = parseFloat(String(str)).toFixed(1);
+
+    return String(float);
+}
+
 readline.question("Enter your height in cm: ", (_height) => {
-    let hgh = _height;
+    let height = _height;
 
     readline.question("Enter your weight in kg: ", (_weight) => {
-        let wgh = _weight;
+        let weight = _weight;
 
-        const height = parseFloat(hgh);
-        const weight = parseFloat(wgh);
+        const BMI = (TurnIntoInt(weight) / TurnIntoInt(height) / TurnIntoInt(height)) * 10000;
+        const FORMATTED = ReadSingularDecimal(BMI);
 
-        const BMI = weight / (height / 100) ** 2;
+        console.log(`\nYour Body Mass Index is: ${FORMATTED}\n`);
 
-        console.log(`\nYour Body Mass Index is: ${BMI}\n`);
-
-        if (BMI <= 18.5) {
+        if (FORMATTED <= 18.5) {
             console.log("Result: You are underweight.");
-        } else if (BMI <= 24.9) {
+        } else if (FORMATTED <= 24.9) {
             console.log("Result: Awesome! You are healthy.");
-        } else if (BMI <= 29.9) {
+        } else if (FORMATTED <= 29.9) {
             console.log("Result: You are overweight.");
         } else {
             console.log("Result: You are obese.");
